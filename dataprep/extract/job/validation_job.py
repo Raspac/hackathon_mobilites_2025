cdefd46832f536fb18e6c6c4ecc4f891bf4db7c9
juliendlt <juju.delmotte@gmail.com>
2025-11-14 00:03:43 +0000

    Refacto global de la partie DataPrep

--- dataprep/extract/job/validation_job.py
+++ dataprep/extract/job/validation_job.py
@@ -1,6 +1,5 @@
 from utils.job_runner import JobRunner
 from utils.loader_local import LoaderLocal
-from utils.transformation import Transformation
 from utils.writer_local import WriterLocal
 import pandas as pd
 
@@ -8,7 +7,6 @@ import pandas as pd
 class ValidationJob(JobRunner):
     def __init__(self):
         self.in_path_list = [
-            "/home/onyxia/work/hackathon_mobilites_2025/data/raw/validations-reseau-ferre-nombre-validations-par-jour-1er-trimestre.parquet",
             "/home/onyxia/work/hackathon_mobilites_2025/data/raw/validations-reseau-ferre-nombre-validations-par-jour-2eme-trimestre.parquet",
             "/home/onyxia/work/hackathon_mobilites_2025/data/raw/validations-reseau-ferre-nombre-validations-par-jour-3eme-trimestre.parquet",
             "/home/onyxia/work/hackathon_mobilites_2025/data/raw/validations-reseau-ferre-nombre-validations-par-jour-2eme-trimestre.parquet"
@@ -21,8 +19,26 @@ class ValidationJob(JobRunner):
             df = LoaderLocal.loader_parquet(file_path)
             dataframes.append(df)
         df_concat = pd.concat(dataframes, ignore_index=True)
+        df_concat['id_zdc'] = pd.to_numeric(df_concat['id_zdc'], errors='coerce')
+        df_concat['nb_vald'] = pd.to_numeric(df_concat['id_zdc'], errors='coerce')
 
-        agg_df = df.groupby('ville').agg(
-            total_ventes=('ventes', 'sum'),
-            premier_responsable=('responsable', lambda x: x.dropna().iloc[0] if not x.dropna().empty else np.nan)
-        ).reset_index()
+        # Total par id_zdc
+        df_total = df_concat.groupby('id_zdc', as_index=False).agg(
+            total_validation=('nb_vald', 'sum')
+        )
+
+        # Total pour la catégorie "Amethyste"
+        df_amethyste = df_concat[df_concat['categorie_titre'] == 'Amethyste'].groupby('id_zdc', as_index=False).agg(
+            total_validation_amethyste=('nb_vald', 'sum')
+            )
+
+        # Merge
+        df_final = pd.merge(df_total, df_amethyste, on='id_zdc', how='inner')
+
+        # Calculer le pourcentage de validation de type amethyste
+        df_final['total_validation_amethyste'] = df_final['total_validation_amethyste'].fillna(0)
+        df_final['pct_amethyste'] = (df_final['total_validation_amethyste'] / df_final['total_validation']) * 100
+        df_final['pct_amethyste'] = df_final['pct_amethyste'].round(4)
+
+        # Ecriture en parquet
+        WriterLocal.write_parquet(df_final, self.out_path)
--- dataprep/extract/run.py
+++
@@ -1,9 +0,0 @@
-from extract.job.carte_pmr_job import CartePmrJob
-from extract.job.ref_gare_idf_job import RefGareIdfJob
-from extract.job.etablissement_job import EtablissementJob
-
-
-def run():
-    CartePmrJob().process()
-    RefGareIdfJob().process()
-    EtablissementJob().process()
